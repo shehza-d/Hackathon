0fdb4457a46dc82cbbb9e059289a5c1967bd8714
Shehzad <shehzaddiqbal@gmail.com>
2023-02-12 09:36:58 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MY GREAT APP
---
+++ WEB/src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+// import Login from "./components/Login";
+// import Signup from "./components/Signup";
+import Home from "./components/home/index";
+// import Courses from "./components/courses/index";
+// import { auth } from "./firebase";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+ 
+export default function App(props) {
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  // const auth = getAuth();
+  // useEffect(() => {
+  //   const Unsubscribe =    onAuthStateChanged(auth, (user) => {
+  //     console.log(user);
+  //     // const uid = user.uid;
+  //     if (user) setIsLogin(true);
+  //     else setIsLogin(false);
+  //   });
+  // // return () => Unsubscribe();
+  // }, []);
+
+  return (
+    <div className="App">
+      {isLogin ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          {/* <Route path="attendance" element={<Attendance />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="students" element={<Students />} /> */}
+          {/* <Route path="profile" element={<Profile />} /> */}
+
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      ) : (
+        <Routes>
+          {/* <Route
+            path="/login"
+            element={<Login isLogin={isLogin} setIsLogin={setIsLogin} />}
+          /> */}
+          {/* <Route path="signup" element={<Signup />} /> */}
+          {/* <Route path="*" element={<Navigate to="/login" replace={true} />} /> */}
+        </Routes>
+      )}
+    </div>
+  );
+}
